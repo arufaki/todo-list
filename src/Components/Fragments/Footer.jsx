65ff2b44173ed1a141e8a37469637050c7328a35
Faki <94790954+Arufaki@users.noreply.github.com>
2023-09-06 14:58:48 +0700

    Update Footer.jsx

--- src/Components/Fragments/Footer.jsx
+++ src/Components/Fragments/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        created by <a href="github.com/arufaki">Arufaki</a> - devChallenges.io
+        created by <a href="https://github.com/Arufaki" target="_blank">Arufaki</a> - devChallenges.io
       </p>
     </footer>
   );
